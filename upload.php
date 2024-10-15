a7d6f68c922baa2b5a4fcf0d4b4d207278da1ed7
AlyssaTifara <tissaalyssa@gmail.com>
2024-10-15 19:25:24 +0700

    pk1

---
+++ upload.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_POST["submit"])) {
+    $targetDirectory = "uploads/";
+    $targetFile = $targetDirectory . basename($_FILES["fileToUpload"]["name"]);
+    $fileType = strtolower(pathinfo($targetFile, PATHINFO_EXTENSION));
+
+    $allowedExtensions = array("jpg", "jpeg", "png", "gif");
+    $maxFileSixe = 5 * 1024 * 1024;
+    
+    if (in_array($fileType, $allowedExtensions) && $_FILES ["fileToUpload"]["size"] <= $maxFileSixe) {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $targetFile)) {
+            echo "File berhasil diunggah";
+        } else {
+            echo "Gagal mengunggah file";
+        }
+    } else {
+        echo "File tidak valid atau melebihi ukuran maksimum yang diizinkan";
+    }
+}
+?>
