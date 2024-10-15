ffec9b5fe5d2073c648d8cd213686126d3eabe8f
AlyssaTifara <tissaalyssa@gmail.com>
2024-10-15 20:30:49 +0700

    pk2

--- form_miltiupload.php
+++ form_miltiupload.php
@@ -1,13 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Multiupload Dokumen</title>
+    <title>Multi-upload Images</title>
 </head>
 <body>
-    <h2>Unggah Dokumen</h2>
-    <form action="proses_upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="file[]" multiple="multiple" accept=".pdf, .doc, .docx" />
-        <input type="submit" value="Unggah" />
+    <h2>Upload Images</h2>
+    <form action="process_image_upload.php" method="post" enctype="multipart/form-data">
+        <input type="file" name="images[]" multiple="multiple" accept=".jpg, .jpeg, .png, .gif" />
+        <input type="submit" value="Upload" />
     </form>
 </body>
 </html>
--- proses_upload.php
+++ proses_upload.php
@@ -1,21 +1,21 @@
 <?php
-$targetDirectory = "documents/";
+$targetDirectory = "uploads/images/";
 
 if (!file_exists($targetDirectory)) {
     mkdir($targetDirectory, 0777, true);
 }
 
-if ($_FILES['files']['name'][0]) {
-    $totalFiles = count($_FILES['files']['name']);
+if ($_FILES['images']['name'][0]) {
+    $totalFiles = count($_FILES['images']['name']);
 
     for ($i=0; $i < $totalFiles; $i++) { 
-        $fileName = $_FILES['files']['name'][$i];
-        $targetFile = $targetDirectory . $targetName;
+        $fileName = $_FILES['images']['name'][$i];
+        $targetFile = $targetDirectory . $fileName;
 
-        if (move_uploaded_file($_FILES['files']['tmp-name'][$i], $targetFile)) {
+        if (move_uploaded_file($_FILES['images']['tmp_name'][$i], $targetFile)) {
             echo "File $fileName berhasil diunggah.<br>";
         } else {
-            echo "Gagal mengunaggah file $fileName.<br>";
+            echo "Gagal mengunggah file $fileName.<br>";
         }
     }
 } else {
