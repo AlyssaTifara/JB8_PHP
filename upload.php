df3b67e741dbb8f16214e02caa8c191d76f08b43
AlyssaTifara <tissaalyssa@gmail.com>
2024-10-16 13:50:32 +0700

    pk1

--- upload.php
+++ upload.php
@@ -10,6 +10,49 @@ if (isset($_POST["submit"])) {
     if (in_array($fileType, $allowedExtensions) && $_FILES ["myfile"]["size"] <= $maxFileSixe) {
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah";
+            
+            $image = null;
+            switch ($fileType) {
+                case 'jpg':
+                case 'jpeg':
+                    $image = imagecreatefromjpeg($targetfile);
+                    break;
+                case 'png':
+                    $image = imagecreatefrompng($targetfile);
+                    break;
+                case 'gif':
+                    $image = imagecreatefromgif($targetfile);
+                    break;
+            }
+            
+            if ($image) {
+                $width = imagesx($image);
+                $height = imagesy($image);
+                $new_width = 200;
+                $new_height = round(($height / $width) * $new_width);
+                
+                $thumb = imagecreatetruecolor(round($new_width), round($new_height));
+                imagecopyresampled($thumb, $image, 0, 0, 0, 0, round($new_width), round($new_height), $width, $height);
+                
+                $thumbnail_path = 'uploads/thumbnail_' . basename($targetfile);
+                switch ($fileType) {
+                    case 'jpg':
+                    case 'jpeg':
+                        imagejpeg($thumb, $thumbnail_path);
+                        break;
+                    case 'png':
+                        imagepng($thumb, $thumbnail_path);
+                        break;
+                    case 'gif':
+                        imagegif($thumb, $thumbnail_path);
+                        break;
+                }
+                
+                imagedestroy($image);
+                imagedestroy($thumb);
+                
+                echo '<br><img src="' . $thumbnail_path . '" alt="Thumbnail">';
+            }
         } else {
             echo "Gagal mengunggah file";
         }
