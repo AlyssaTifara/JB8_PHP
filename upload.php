8fc221aff3e41c0cd5b3a1e614c32853764d4436
AlyssaTifara <tissaalyssa@gmail.com>
2024-10-16 12:05:15 +0700

    pk1

--- upload.php
+++ upload.php
@@ -1,7 +1,7 @@
 <?php
 if (isset($_POST["submit"])) {
-    $targetDirectory = "uploads/";
-    $targetfile = $targetdir . basename($_FILES["fileToUpload"]["name"]);
+    $targetdir = "uploads/";
+    $targetfile = $targetdir . basename($_FILES["myfile"]["name"]);
     
         if (move_uploaded_file($_FILES["myfile"]["tmp_name"], $targetfile)) {
             echo "File berhasil diunggah";
